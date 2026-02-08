fbab49f6aa61b06e69efee480b9fd27af5ac43d2
fk-18 <K256518@nu.edu.pk>
2026-02-08 23:24:21 +0500

    Task 7: Declare variables and print using correct data types

--- intro.c
+++ intro.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Name:\tFatima Kaleem Khan\n");
-    printf("Roll Number:\t25K-6518\n");
-    printf("Section:\tSpring 2026\n");
+    int age = 18;
+    float height = 5.4f;
+    double gpa = 3.75;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Height: %.1f\n", height);
+    printf("GPA: %.2lf\n", gpa);
+    printf("Grade: %c\n", grade);
+
     return 0;
 }
